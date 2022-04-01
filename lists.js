d014ed2c572af4c59e394d05eae9074e672003cc
Bryan Castillo <bcastillo@fractalinside.com>
2022-03-31 18:42:30 -0600

    creating an advanced example with lists

--- lists.js
+++ lists.js
@@ -2,30 +2,46 @@ const vue = new Vue({
 	el: '#main'
 	, data: {
 		id: 0
-		, allTodos:[]
+		, allTodos: []
+		, createdTodos:[]
+		, inProgreesTodos: []
+		, errorTodos: []
+		, finishedTodos: []
 		, newTodo: null
+		, statuses: {
+			CREATED: 1
+			, IN_PROGRESS: 2
+			, ERORR: 3
+			, FINISHED: 4
+		}
 	}
 	, methods: {
 		addTodo() {
-			if(this.newTodo.trim())
-				this.allTodos.push({id: this.id++, title: this.newTodo.trim(), completed: false});
+			if(this.newTodo)
+				this.allTodos.push({
+					id: this.id++
+					, title: this.newTodo.trim()
+					, progress: 0
+					, status: this.statuses.CREATED
+				});
 			this.newTodo = null;
-		}
-		, completeTodo(id) {
-			const todo = this.getTodo(id)[0];
-			todo.completed = true;
-		}
-		, uncompleteTodo(id) {
-			const todo = this.getTodo(id)[0];
-			todo.completed = false;
 		}
 		, getTodo(id) {
 			return this.allTodos.filter(item => item.id === id);
 		}
 	}
 	, computed: {
-		showCompleted(){
-			return this.allTodos.filter(item => item.completed);
+		showCreated(){
+			return this.allTodos.filter(item => item.status === this.statuses.CREATED);
+		}
+		, showInProgress(){
+			return this.allTodos.filter(item => item.status === this.statuses.IN_PROGRESS);
+		}
+		, showError(){
+			return this.allTodos.filter(item => item.status === this.statuses.ERORR);
+		}
+		, showFinished(){
+			return this.allTodos.filter(item => item.status === this.statuses.FINISHED);
 		}
 	}
 
